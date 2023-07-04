49b434f61619e6e9eaa5cbfa1f066a92c099e4a3
arale2900 <arale2900@gmail.com>
2023-07-04 15:48:31 +0200

    Creating Db  and moving whole project to docker image. Using docker-compose.yml

--- TestForum.API/Models/Article.cs
+++ TestForum.API/Models/Article.cs
@@ -1,10 +1,15 @@
-﻿namespace TestForum.API.Models
+﻿using Microsoft.Identity.Client;
+
+namespace TestForum.API.Models
 {
 	public class Article
 	{
+		public Guid Id { get; set; }
 		public string Title { get; set; }
 		public string Description { get; set; }
 		public string Author { get; set; }
+		public DateTime PublicationTime { get; set; }
+		public List<Comment> Comments { get; set; }
 		public Article() { }
 	}
 }
--- TestForum.API/Program.cs
+++ TestForum.API/Program.cs
@@ -1,11 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.OpenApi.Models;
 using TestForum.API.Abstract;
 using TestForum.API.Services;
+using TestForum.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+	options.UseSqlServer(
+		builder.Configuration.GetConnectionString("DefaultConnection"),
+		x => x.MigrationsAssembly("TestForum.DatabaseContext")));
 
 builder.Services.AddScoped<IUsersService, ExampleUsersService>();
 

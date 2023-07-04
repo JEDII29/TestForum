43fc3c6cb1242e20ff500432a23d590203b9339a
JEDII-laptop <j.szczerbal29@gmail.com>
2023-07-04 16:14:05 +0200

    Controllers and services organization

--- TestForum.API/Models/Article.cs
+++ TestForum.API/Models/Article.cs
@@ -4,7 +4,10 @@
 	{
 		public string Title { get; set; }
 		public string Description { get; set; }
-		public string Author { get; set; }
+		public int AuthorID { get; set; }
+		public string AuthorName {get; set; }
+		public DateTime PublishionDate {get; set; }
+		private State Status {get; set; }
 		public Article() { }
 	}
 }
